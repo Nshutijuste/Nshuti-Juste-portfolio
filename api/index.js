6d4c95c27b737baf1d21306007c63ed715ae039c
Nshuti Juste <nshutiijuste@gmail.com>
2026-09-21 19:41:45 +0200

    again

--- api/index.js
+++ api/index.js
@@ -1,2 +1,5 @@
 'use strict';
+
+const express = require('express');
+void express;
 module.exports = require('../backend/app');
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 'use strict';
 
+const express = require('express');
+void express;
 module.exports = require('./backend/app');
